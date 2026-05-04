d9586a4b6d89ee48f7e3979e15f49c990a5cd2b2
Caua47 <caua.vinicius24@nave.org.br>
2026-05-04 08:40:55 -0300

    primeiro

---
+++ arma.gd
@@ -0,0 +1,12 @@
+extends Node3D
+var vel = 1000
+var dir = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += vel * dir * delta
---
+++ character_body_3d.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+
+const SPEED = 3
+
+func _physics_process(delta: float) -> void:
+	var direcao = Input.get_vector("esquerda", "direita", "cima", "baixo") * SPEED
+	velocity = Vector3(direcao.x, velocity.y, direcao.y).rotated(Vector3.UP, $"%CharacterBody3D/character-male-f/Camera3D".rotation.y)
+	move_and_slide()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		$"%CharacterBody3D/character-male-f/Camera3D".rotation.y += -event.relative.x * 0.01
+		$"%CharacterBody3D/character-male-f/Camera3D".rotation.x += -event.relative.y * 0.01
+		$"%CharacterBody3D/character-male-f/Camera3D".rotation.x = clamp($"%CharacterBody3D/character-male-f/Camera3D".rotation.x, deg_to_rad(-30), deg_to_rad(60))
+func atirar_raio_da_camera_fps(mouse_pos: Vector2):
+
+	var camera = get_viewport().get_camera_3d()
+	var origin = camera.project_ray_origin(mouse_pos)
+	var end = origin + camera.project_ray_normal(mouse_pos) * 1000
+	var space_state = get_world_3d().direct_space_state
+
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.exclude = [%CharacterBody3D]
+
+	var result = space_state.intersect_ray(query)
+
+	if result.is_empty():
+		return null
+	else:
+		return result
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		atirar_projetil(event.position)
+
+func atirar_projetil(from):
+	var alvo = atirar_raio_da_camera_fps(from)
+	if alvo != null:
+		if alvo.collider.has_method("dano"):
+			alvo.collider.dano(50)
